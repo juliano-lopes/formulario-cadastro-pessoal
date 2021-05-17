81a7f2d059db4ea885fd4f38ab3ae568f4059911
Juliano Lopes <julopeson@gmail.com>
2021-05-17 00:24:54 -0300

    Cria arquivo para funções de cep

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -2,33 +2,3 @@
       
       return false;
     }
-    const isValidCep = (cep)=>(cep!=""&&/^[0-9]{8}$/.test(cep));
-    const fillAdress = (adressData)=>{
-      console.log("cep veio: ",adressData);
-      document.getElementById("endereco").value=adressData.logradouro;
-        document.getElementById("bairro").value=adressData.bairro;
-      document.getElementById("cidade").value=adressData.localidade;
-      document.getElementById("uf").value=adressData.uf;
-    }
-    const cleanAdress = (adressData)=>{
-      
-      document.getElementById("endereco").value="";
-        document.getElementById("bairro").value="";
-      document.getElementById("cidade").value="";
-      document.getElementById("uf").value="";
-    }
-    const getAdressByCep =(cep)=>{
-      console.log("chamou");
-    if(isValidCep(cep)){
-      console.log("is valid");
-      let url=`https://viacep.com.br/ws/${cep}/json/?callback=fillAdress`;
-      console.log("buscando cep "+url);
-    let script = document.createElement("script");
-    script.src=url;
-    document.body.appendChild(script);
-    }
-    else{
-      alert("CEP inválido...");
-    cleanAdress();
-    }
-    }
