879261f9dc8420cfc33f4ca3d6fa4cca8a27cd09
Juliano Lopes <julopeson@gmail.com>
2021-05-17 00:15:54 -0300

    Cria função para preencher e apagar campos de endereço

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -3,3 +3,18 @@
       return false;
     }
     const isValidCep = (cep)=>(cep!=""&&/^[0-9]{8}$/.test(cep));
+    const fillAdress = (adressData)=>{
+      console.log("cep veio: ",adressData);
+      document.getElementById("endereco").value=adressData.logradouro;
+        document.getElementById("bairro").value=adressData.bairro;
+      document.getElementById("cidade").value=adressData.localidade;
+      document.getElementById("uf").value=adressData.uf;
+    }
+    const cleanAdress = (adressData)=>{
+      
+      document.getElementById("endereco").value="";
+        document.getElementById("bairro").value="";
+      document.getElementById("cidade").value="";
+      document.getElementById("uf").value="";
+    }
+    
