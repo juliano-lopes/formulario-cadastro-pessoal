0751e4b241605e65ad0fe12d6eb7345000ccd76e
Juliano Lopes <julopeson@gmail.com>
2021-05-17 00:22:11 -0300

    Adiciona listener no campo cep e inclui scripts no index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,9 @@ window.onload = ()=>{
 
     const form = document.querySelector("form");
     form.onsubmit= formValidation;
+    const cep = document.getElementById("cep");
+    cep.addEventListener("change",(e)=>{
+getAdressByCep(e.target.value);
+    });
+
     }
