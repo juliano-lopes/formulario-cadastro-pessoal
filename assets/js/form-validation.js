7497a7df57712328cc82e26625c1b9256a5353b7
Juliano Lopes <julopeson@gmail.com>
2021-05-18 01:01:37 -0300

    Adiciona validação de cpf e celular na função chamada por onsubmit do formulário.

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,4 +1,17 @@
     const formValidation = ()=>{
-      
+if(!cpfValidation(document.getElementById("cpf").value)){
+  alert("CPF inválido...");
+  document.getElementById("cpf").focus();
+  return false;
+}
+if(!isValidCelPhone(document.getElementById("cel").value)){
+  alert("Número de celular inválido, não se esqueça de informar o DDD sem o 0");
+  document.getElementById("cel").focus();
+  return false;
+}
+
       return false;
     }
+
+const isValidCelPhone = (celPhone)=>(celPhone!=""&&/^[0-9]{11}$/.test(celPhone));
+const isValidPhone = (phone)=>(phone!="" ? /^[0-9]{10}$/.test(phone) : true);
