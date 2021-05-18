f94f8d78dc9444448da43c6af5562dff02298f14
Juliano Lopes <julopeson@gmail.com>
2021-05-18 00:59:47 -0300

    Adiciona onchange nos campos celular, telefone e cpf e cep.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,30 @@ window.onload = ()=>{
 
     const form = document.querySelector("form");
     form.onsubmit= formValidation;
+const cpf = document.getElementById("cpf");
+    cpf.onchange=(e)=>{
+      if(!cpfValidation(e.target.value)){
+        alert("CPF inválido...");
+      }
+      }
+      
     const cep = document.getElementById("cep");
-    cep.addEventListener("change",(e)=>{
+    cep.onchange = (e)=>{
 getAdressByCep(e.target.value);
-    });
+    }
+    const phone = document.getElementById("tel");
+phone.onchange=(e)=>{
+  if(!isValidPhone(e.target.value)){
+    alert("Telefone fixo inválido, não se esqueça de informar o DDD sem o 0");
+  }
+}
+
+  const celPhone = document.getElementById("cel");
+  celPhone.onchange=(e)=>{
+    if(!isValidCelPhone(e.target.value)){
+      alert("Número de celular inválido, não se esqueça de informar o DDD sem o 0");
+    }
+  }
+
 
     }
